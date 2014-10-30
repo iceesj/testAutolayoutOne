360d230344efd964d12315769bde1533b0431c0a
csjwork <icsj@icloud.com>
2014-10-30 15:44:43 +0800

    auto layout test one

---
+++ testAutoOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testAutoOne
+//
+//  Created by iceesj on 14/10/27.
+//  Copyright (c) 2014年 iceesj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
